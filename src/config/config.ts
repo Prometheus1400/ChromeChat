140794f03e16a4b5ebd7a7f1202282ee6b3dfb90
Prometheus1400 <prometheus1400@gmail.com>
2023-01-02 10:25:01 -0600

    lighter avatar colors, divider from top bar

--- src/config/config.ts
+++ src/config/config.ts
@@ -124,10 +124,13 @@ export const friendRequestRef = firestore
     .withConverter(friendRequestConverter)
 
 export function getRandomColor() {
-    var letters = "0123456789ABCDEF"
-    var color = "#"
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
-    }
+    // var letters = "0123456789ABCDEF"
+    // var color = "#"
+    // for (var i = 0; i < 6; i++) {
+    //     color += letters[Math.floor(Math.random() * 16)]
+    // }
+    // return color
+
+    const color = "hsl(" + Math.random() * 360 + ", 100%, 75%)"
     return color
 }
